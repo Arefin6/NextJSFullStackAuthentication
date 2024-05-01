33a9b000c17b020f604f4783b5cb1a6ed0f9b174
arefin6 <arefinhossain3@gmail.com>
2024-05-01 22:48:26 +0600

    logout complete

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+export async function GET() {
+  try {
+    const response = NextResponse.json({
+      message: "Logout Successful",
+      success: true,
+    });
+    response.cookies.set("token", "", {
+      httpOnly: true,
+    });
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,45 @@
-export default function ProfilePage(){
-    return(
-       <div>
-        <h1>Profile</h1>
-       </div> 
-    )
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = useState("nothing");
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout Successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
+
+      {/* <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >GetUser Details</button> */}
+    </div>
+  );
 }
